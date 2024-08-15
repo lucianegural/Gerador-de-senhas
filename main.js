709d0e876798a5861749db26e00bd1443504ce27
lucianegural <126890538+lucianegural@users.noreply.github.com>
2024-08-15 09:16:49 -0300

    arquivo js criado

---
+++ main.js
@@ -0,0 +1,31 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textcontent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+    // tamanhosenha = tamanhoSenha+1;
+    tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    gerasenha();
+}
